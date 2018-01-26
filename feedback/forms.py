52445c9cc96250ffa79558eb031c3976b9f8c484
atedeev <atedeev@localhost>
2018-01-27 02:06:45 +0300

    add  client ipaddr in django feedback form

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,7 +1,9 @@
 from django.forms import ModelForm
 from .models import Feedback
+from django import forms
 
 class FeedbackForm(ModelForm):
     class Meta:
         model = Feedback
-        exclude = ['datetime', 'ip']
+        exclude = ['datetime']
+        widgets = {'ip': forms.HiddenInput()}
--- feedback/views.py
+++ feedback/views.py
@@ -4,11 +4,20 @@ from .forms import FeedbackForm
 # Create your views here.
 
 def index(request):
+    def get_client_ip(request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip=x_forwarded_for.split(',')[-1].strip()
+        else:
+            ip=request.META.get('REMOTE_ADDR')
+        return  ip
     if request.method == 'POST':
         form = FeedbackForm(request.POST)
         context = {"form" : form}
         if form.is_valid():
-            form.save()
+            aform = form.save(commit=False)
+            aform.ip = get_client_ip(request)
+            aform.save()
             context["thanks"] = True
             return render(request, 'feedback/index.html', context)
     else:
